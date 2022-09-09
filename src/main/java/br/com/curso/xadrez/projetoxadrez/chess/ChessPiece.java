dbc725c59832f28eaa4b2b3dcb9ef795f0172762
Mythalie <mythalie.sandretti@iteris.com.br>
2022-09-09 11:23:27 -0300

    contagem de movimentos das peças

--- src/main/java/br/com/curso/xadrez/projetoxadrez/chess/ChessPiece.java
+++ src/main/java/br/com/curso/xadrez/projetoxadrez/chess/ChessPiece.java
@@ -3,15 +3,25 @@ package br.com.curso.xadrez.projetoxadrez.chess;
 import br.com.curso.xadrez.projetoxadrez.boardgame.Board;
 import br.com.curso.xadrez.projetoxadrez.boardgame.Piece;
 import br.com.curso.xadrez.projetoxadrez.boardgame.Position;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 public abstract class ChessPiece extends Piece {
     private Color color;
+    private int moveCount;
 
     public ChessPiece(Board board, Color color) {
         super(board);
         this.color = color;
+    }
+
+    public void increaseMoveCount() {
+        moveCount++;
+    }
+
+    public void decreaseMoveCount() {
+        moveCount--;
     }
 
     public ChessPosition getChessPosition() {
